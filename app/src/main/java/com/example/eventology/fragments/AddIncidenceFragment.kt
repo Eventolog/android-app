2bb95ef703025a4688e76e7f642484d77a9c546e
j <anonymosmail@gmail.com>
2025-05-16 14:30:46 +0200

    add logic at createIncidences page

--- app/src/main/java/com/example/eventology/fragments/AddIncidenceFragment.kt
+++ app/src/main/java/com/example/eventology/fragments/AddIncidenceFragment.kt
@@ -4,27 +4,57 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.eventology.R
-import com.example.eventology.databinding.FragmentPageIncidencesBinding
+import com.example.eventology.data.services.ApiServiceProvider
+import com.example.eventology.databinding.FragmentAddIncidenceBinding
+import kotlinx.coroutines.launch
 
 /**
- * Fragment that displays a form to create incidence
+ * Fragment that displays a form to create an incidence.
  *
  * @property authenticatedLayoutFragment Used for navigating from this fragment.
  */
 class AddIncidenceFragment(
     private val authenticatedLayoutFragment: AuthenticatedLayoutFragment
-) : PageFragments(4, authenticatedLayoutFragment){
-    private var _binding: FragmentPageIncidencesBinding? = null
+) : PageFragments(4, authenticatedLayoutFragment) {
+
+    private var _binding: FragmentAddIncidenceBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPageIncidencesBinding.inflate(inflater, container, false)
-        inflater.inflate(R.layout.fragment_main, container, false)
+        // Correct binding for this layout
+        _binding = FragmentAddIncidenceBinding.inflate(inflater, container, false)
 
+        binding.addIncidenceButton.setOnClickListener {
+            var reason = binding.inputText.text.toString()
+
+            when {
+                reason.isEmpty() -> {
+                    Toast.makeText(requireContext(), getString(R.string.reason_empty), Toast.LENGTH_SHORT).show()
+                }
+                reason.length > 500 -> {
+                    Toast.makeText(requireContext(), getString(R.string.reason_too_long), Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    lifecycleScope.launch {
+
+                    Toast.makeText(requireContext(), getString(R.string.incidence_submitted), Toast.LENGTH_SHORT).show()
+                    // create incidence
+                    ApiServiceProvider.getDataService().createIncidence(reason)
+                    // return to incidences page
+                    authenticatedLayoutFragment.goBack()
+                        }
+                }
+            }
+
+
+        }
         return binding.root
     }
 
